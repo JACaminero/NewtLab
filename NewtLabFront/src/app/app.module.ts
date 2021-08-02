89cb8a909ae1c62d9ff0d48070effdb1944228af
JACaminero <juanc-r1@hotmail.com>
2021-08-02 13:27:13 -0400

    commiting frontend again

---
+++ NewtLabFront/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { PixiCanvasComponent } from './components/experiment/pixi-canvas/pixi-canvas.component';
+import { DescriptionComponent } from './components/experiment/description/description.component';
+import { InstructionsComponent } from './components/experiment/instructions/instructions.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeAdminComponent } from './components/home-admin/home-admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    NavbarComponent,
+    PixiCanvasComponent,
+    DescriptionComponent,
+    InstructionsComponent,
+    HomeComponent,
+    HomeAdminComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatSelectModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
