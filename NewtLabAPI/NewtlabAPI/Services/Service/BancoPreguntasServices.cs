d00b5a7f9981e66fa7aa246c1efd0fcba543f75a
JACaminero <juanc-r1@hotmail.com>
2021-09-26 21:08:48 -0400

    fixing

--- NewtLabAPI/NewtlabAPI/Services/Service/BancoPreguntasServices.cs
+++ NewtLabAPI/NewtlabAPI/Services/Service/BancoPreguntasServices.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NewtlabAPI.Models;
+using NewtlabAPI.Services.IServices;
+
 namespace NewtlabAPI.Services
 {
-    public class BancoPreguntasServices
+    public class BancoPreguntasServices : IBancoPreguntaService
     {
         public BancoPreguntasServices()
         {
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BancoPregunta> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BancoPregunta> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(BancoPregunta bancoPregunta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(BancoPregunta bancoPregunta)
+        {
+            throw new NotImplementedException();
         }
     }
 }
