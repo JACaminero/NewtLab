1093b54d193652903292b58fca8b56b42ab19f13
JACaminero <juanc-r1@hotmail.com>
2021-09-26 22:39:22 -0400

    ó.ó

---
+++ NewtLabAPI/NewtLabAPI/Services/IServices/IBancoPreguntaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NewtlabAPI.Models;
+
+namespace NewtlabAPI.Services.IServices
+{
+    public interface IBancoPreguntaService
+    {
+        IEnumerable<BancoPregunta> GetAll();
+        Task Insert(BancoPregunta bancoPregunta);
+        Task<BancoPregunta> GetById(int id);
+        bool Update(BancoPregunta bancoPregunta);
+        bool Delete(int id);
+    }
+}
